d0b68f31355bde5f162fe4a2380b09003ca96fcd
DanilovAI <aleksdanilov2015@gmail.com>
2025-12-10 23:29:51 +0700

    Добавлена ЛБ1

---
+++ Lab1_OOP/Model/Person.cs
@@ -0,0 +1,114 @@
+﻿namespace Model
+{
+    public class Person
+    {
+        /// <summary>
+        /// Имя
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        private string _surname;
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        private int _age;
+
+        /// <summary>
+        /// Конструктор класса Person
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        /// <param name="age"></param>
+        /// <param name="gender"></param>
+        public Person(string name, string surname, int age, Gender gender)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Gender = gender;
+        }
+
+        /// <summary>
+        /// Свойство Имя
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception($"{nameof(Name)}" +
+                        $" can't be null or empty!");
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Свойство Фамилия
+        /// </summary>
+        public string Surname
+        {
+            get { return _surname; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception($"{nameof(Surname)}" +
+                        $" can't be null or empty!");
+                }
+                else
+                {
+                    _surname = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Свойство Возраст
+        /// </summary>
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                const int minAge = 0;
+                const int maxAge = 123;
+                if (value < minAge || value > maxAge)
+                {
+                    throw new Exception($"{nameof(Age)} can't be less" +
+                        $" {minAge} or above {maxAge}!");
+                }
+                else
+                { 
+                    _age = value;                
+                }
+            }
+        }
+
+        /// <summary>
+        /// Свойство Пол
+        /// </summary>
+        public Gender Gender { get; set; }
+
+        /// <summary>
+        /// Функция для вывода данных о человеке
+        /// </summary>
+        /// <param name="person"></param>
+        public void PrintPersonInfo(Person person)
+        {
+            Console.WriteLine($"Имя - {Name}, Фамилия - {Surname}" +
+                $"Возраст - {Age}, Пол - {Gender}.");
+        }
+
+
+    }
+}
---
+++ Lab1_OOP/Model/PersonList.cs
@@ -0,0 +1,21 @@
+﻿namespace Model
+{
+    public class PersonList
+    {
+        /// <summary>
+        /// Список
+        /// </summary>
+        public List<Person> list;
+
+        /// <summary>
+        /// Конструктор списка с одним значением
+        /// </summary>
+        /// <param name="person"></param>
+        public PersonList(Person person) 
+        {
+            list = [person];
+        }
+
+
+    }
+}
